a7471c5eecdf772bd1384cfb628e790500876232
Fabrizio <whoet.wo@gmail.com>
2020-11-04 16:47:10 +0100

    Fix JS e rimozione ID

--- js/main.js
+++ js/main.js
@@ -1,29 +1,4 @@
-$('#azienda').click(function(){
-   $('.azienda-utility').siblings().hide()
-   $('.azienda-utility').toggle();
-})
-
-$('#tariffe').click(function(){
-   $('.tariffe-utility').siblings().hide()
-   $('.tariffe-utility').toggle();
-})
-
-$('#aiuto').click(function(){
-   $('.aiuto-utility').siblings().hide()
-   $('.aiuto-utility').toggle();
-})
-
-$('#conti').click(function(){
-   $('.conti-utility').siblings().hide()
-   $('.conti-utility').toggle();
-})
-
-$('#lingua').click(function(){
-   $('.lingua-utility').siblings().hide()
-   $('.lingua-utility').toggle();
-})
-
-$('#prodotti').click(function(){
-   $('.prodotti-utility').siblings().hide()
-   $('.prodotti-utility').toggle();
+$('.nav-right a').click(function(){
+   $(this).parent().siblings().children('.menu').hide()
+   $(this).siblings().toggle()
 })
