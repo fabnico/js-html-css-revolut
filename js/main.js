dd2d36e1a051469cd66e3c77bdb5525ef3d3efe3
Fabrizio <whoet.wo@gmail.com>
2020-11-04 17:28:54 +0100

    posizionamento corretto menu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $('.nav-right a').click(function(){
    $(this).parent().siblings().children('.menu').hide()
+   console.log($(this).parent().siblings().children('.menu'))
    $(this).siblings().toggle()
 })
